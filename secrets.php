92d6678440f0f22db66c0f2817a8974023d3036f
Andrew Ry <RyAndrew@users.noreply.github.com>
2021-04-27 09:27:49 -0500

    add secrets.php

---
+++ secrets.php
@@ -0,0 +1,14 @@
+<?php
+
+$redirectUrl = 'https://example.com/login/';
+
+$office365_tenant = '11111111-1111-1111-1111-111111111111';
+$office365_appId = '11111111-1111-1111-1111-111111111111';
+$office365_cert = $_SERVER['DOCUMENT_ROOT'] . '/office365.pem';
+
+$okta_provider = 'https://example.okta.com/';
+$okta_appId = '11111111111111111111';
+$okta_secret = '11111111111111111-11111111111111111-1111';
+
+$github_appId = '11111111111111111111';
+$github_secret = '1111111111111111111111111111111111111111';
